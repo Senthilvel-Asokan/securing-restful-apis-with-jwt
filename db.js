a263f17965af45c67c6455c0273b22a1533eea85
Asokan <XBBL5T9@pac.bnymellon.net>
2018-07-23 17:43:09 +0530

    returns email status on deletion

--- db.js
+++ db.js
@@ -1,3 +1,3 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://test@test.com:Password_123@ds018268.mlab.com:18268/securing-restful-api-with-jwt', {useNewUrlParser: true } );
+mongoose.connect('mongodb://test%40test.com:P%40ssword_123@ds249311.mlab.com:49311/securing-restful-api-with-jwt', { useNewUrlParser: true } );
 //mongoose.connect('mongodb://Username:Password@cluster0-shard-00-00-jueel.mongodb.net:27017/test?replicaSet=Cluster0-shard-0', { useNewUrlParser: true } );
